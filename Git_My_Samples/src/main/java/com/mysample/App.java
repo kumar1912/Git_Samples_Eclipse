b409e2ba745b6f007a73416206fd04af77c3118a
Kumar D <kumar.dodda@gmail.com>
2019-10-02 18:12:21 +0530

    updated now

--- Git_My_Samples/src/main/java/com/mysample/App.java
+++ Git_My_Samples/src/main/java/com/mysample/App.java
@@ -6,6 +6,11 @@ package com.mysample;
  */
 public class App  {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "1.Hello World!" );
+        System.out.println( "2.Hello World!" );
+        System.out.println( "3.Hello World!" );
+        System.out.println( "4.Hello World!" );
+        System.out.println( "5.Hello World!" );
+        System.out.println( "6.Hello World!" );
     }
 }
