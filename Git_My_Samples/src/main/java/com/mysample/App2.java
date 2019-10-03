012506258304775889684be94d9519c68ace927a
Kumar D <kumar.dodda@gmail.com>
2019-10-03 08:55:15 +0530

    Added App2 + Changes

--- Git_My_Samples/src/main/java/com/mysample/App2.java
+++ Git_My_Samples/src/main/java/com/mysample/App2.java
@@ -4,7 +4,7 @@ package com.mysample;
  * Hello world!
  *
  */
-public class App  {
+public class App2  {
     public static void main( String[] args ) {
         System.out.println( "1.Hello World!" );
         System.out.println( "2.Hello World!" );
